15a1886f6f51102ca6154b82fcfe196dd18b7fb1
Michiel Leyman <michiel.leyman@gmail.com>
2017-07-13 10:34:30 +0200

    Landing page for root URL

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,7 +46,11 @@
             }
 
             .title {
-                font-size: 84px;
+                width: 200px;
+            }
+
+            .title img {
+                width: 100%;
             }
 
             .links > a {
@@ -66,28 +70,13 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <img src="img/prisma-logo.png">
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="/api/v1/">API Version 0.0.0</a>
                 </div>
             </div>
         </div>
