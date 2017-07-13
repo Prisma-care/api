1221e89d5589fb249393dd7a0d57412d2b423530
Simon westerlinck <simon_westen@hotmail.com>
2017-07-13 14:53:14 +0200

    Add patient profile model, controller and migration

---
+++ database/migrations/2017_07_13_124412_create_profiles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->datetime('date_of_birth');
+            $table->string('birth_location');
+            $table->string('location');
+            $table->string('care_house');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
