d9141103195c4d5a8082b85e8a61576829b7ed61
Michiel Leyman <michiel.leyman@gmail.com>
2017-07-19 11:50:27 +0200

    Add a working JWT sign in

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,12 +4,42 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use JWTAuth;
 
 class LoginController extends Controller
 {
 
     public function signin(Request $request)
     {
-        //
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        try {
+            if (! $token = JWTAuth::attempt($credentials)) {
+                $response = response()->json([
+                    'code' => '401',
+                    'message' => 'Invalid credentials'
+                ], 401);
+            } else {
+                $response = response()->json([
+                    'meta' => [
+                        'code' => '200',
+                        'message' => 'OK'
+                    ],
+                    'token' => $token
+                ], 200);
+            }
+        } catch (JWTException $e) {
+            $response = response()->json([
+                'code' => '500',
+                'message' => 'Unexpected error logging in the user'
+            ], 500);
+        }
+
+        return $response;
     }
 }
