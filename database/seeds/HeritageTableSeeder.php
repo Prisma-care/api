4a7f2664e6df8cf3c695518abad2de39b88f455e
Michiel Leyman <MichielLeyman@users.noreply.github.com>
2017-07-24 16:30:10 +0200

    Store heritage assets on disk (#67)
    * Change asset_name to be unique
    
    * Add Food to seeder and unignore assets
    
    * Add heritage assets

--- database/seeds/HeritageTableSeeder.php
+++ database/seeds/HeritageTableSeeder.php
@@ -13,7 +13,7 @@ class HeritageTableSeeder extends Seeder
     public function run()
     {
         $heritage = Heritage::create(
-        	['asset_name' => '40s1.jpg',
+        	['asset_name' => '1.jpg',
         	'asset_type' => 'image',
         	'description' => 'King Leopold and Princess Lilian',
         	'happened_at' => '1942-01-01',
@@ -22,7 +22,7 @@ class HeritageTableSeeder extends Seeder
 
         $heritage = Heritage::create(
         	[
-        	'asset_name' => '40s2.jpg',
+        	'asset_name' => '2.jpg',
         	'asset_type' => 'image',
         	'description' => 'Two German motorcyclists armed with MP 40s follow a King Tiger',
         	'happened_at' => 'december 1944',
@@ -31,7 +31,7 @@ class HeritageTableSeeder extends Seeder
         
         $heritage = Heritage::create(
         	[
-        	'asset_name' => '40s3.jpg',
+        	'asset_name' => '3.jpg',
         	'asset_type' => 'image',
         	'description' => 'Infantrymen in the South Saskatchewan Regiment',
         	'happened_at' => 'september 1944',
@@ -40,7 +40,7 @@ class HeritageTableSeeder extends Seeder
         
         $heritage = Heritage::create(
         	[
-        	'asset_name' => '50s1.jpg',
+        	'asset_name' => '4.jpg',
         	'asset_type' => 'image',
         	'description' => 'Tramway in Borinage',
         	'happened_at' => '1959',
@@ -49,7 +49,7 @@ class HeritageTableSeeder extends Seeder
         
         $heritage = Heritage::create(
         	[
-        	'asset_name' => '50s2.jpg',
+        	'asset_name' => '5.jpg',
         	'asset_type' => 'image',
         	'description' => 'Graslei Gent',
         	'happened_at' => '1954',
@@ -58,7 +58,7 @@ class HeritageTableSeeder extends Seeder
         
         $heritage = Heritage::create(
         	[
-        	'asset_name' => '60s1.jpg',
+        	'asset_name' => '6.jpg',
         	'asset_type' => 'image',
         	'description' => 'Mode in de jaren 60',
         	'happened_at' => '1960',
@@ -67,7 +67,7 @@ class HeritageTableSeeder extends Seeder
         
         $heritage = Heritage::create(
         	[
-        	'asset_name' => '60s2.jpg',
+        	'asset_name' => '7.jpg',
         	'asset_type' => 'image',
         	'description' => 'Mars tegen de oorlog in Vietnam',
         	'happened_at' => '1964',
@@ -76,7 +76,7 @@ class HeritageTableSeeder extends Seeder
         
         $heritage = Heritage::create(
         	[
-        	'asset_name' => '60s3.jpg',
+        	'asset_name' => '8.jpg',
         	'asset_type' => 'image',
         	'description' => 'De rede van bij elkaar 17 minuten hield Martin Luther King voor het Lincoln Memorial in Washington D.C. De woorden van King inspireerden niet alleen de bezoekers ter plekke, maar ook elders in de wereld en in latere tijden. De rede hield hij ter gelegenheid van de Mars naar Washington die die dag in de stad was aangekomen, ten overstaan van een publiek van meer dan 200.000 mensen. Van de rede werd rechtstreeks verslag uitgebracht op televisie.',
         	'happened_at' => '28 augustus 1963',
@@ -85,7 +85,7 @@ class HeritageTableSeeder extends Seeder
         
         $heritage = Heritage::create(
         	[
-        	'asset_name' => '60s4.jpg',
+        	'asset_name' => '9.jpg',
         	'asset_type' => 'image',
         	'description' => 'Moonlanding in 1969. Aldrin next to the Passive Seismic Experiment Package with Eagle in the background',
         	'happened_at' => '20 juli 1969',
@@ -94,7 +94,7 @@ class HeritageTableSeeder extends Seeder
         
         $heritage = Heritage::create(
         	[
-        	'asset_name' => 'Rodenbachsfeesten.jpg',
+        	'asset_name' => '10.jpg',
         	'asset_type' => 'image',
         	'description' => 'Affiche naar aanleiding van de Rodenbachfeesten van 21 tot 23 augustus en 12 september 1909 met onder meer liederavond, onthulling standbeeld, cantate, stoet, vuurwerk, studentenlanddag, toneel, feest voor Ernest Van Dyck, ... uitgevaardigd door het stadsbestuur en het feestcomité. De affiche toont de afbeelding van een vrouw in geel gewaad met een Vlaamse leeuw op de borst. zij staat aan het roer van een schip met opschrift vliegt de blauwvoet. Voor haar staat een engel. Op de achtergrond de skyline van Roeselare. De affiche is getekend door Gaston Vallaeys.',
         	'happened_at' => 'augustus/september 1909',
@@ -103,7 +103,7 @@ class HeritageTableSeeder extends Seeder
         
         $heritage = Heritage::create(
         	[
-        	'asset_name' => 'SportstadiumIngelmunster.jpg',
+        	'asset_name' => '11.jpg',
         	'asset_type' => 'image',
         	'description' => 'Opening sportstadion Ingelmunster',
         	'happened_at' => '1978',
@@ -112,7 +112,7 @@ class HeritageTableSeeder extends Seeder
         
         $heritage = Heritage::create(
         	[
-        	'asset_name' => 'sport2.jpg',
+        	'asset_name' => '12.jpg',
         	'asset_type' => 'image',
         	'description' => 'Wielerwedstrijd Brussel-Izegem gewonnen door L. Vandaele. Deze foto maakt deel uit van de collectie van persfotograaf Maurice Terryn, die Izegem portretteerde vanaf de jaren 1950 tot eind de jaren 1990. De volledige collectie (meer dan 35.000 negatieven) werd door Stad Izegem aangekocht.',
         	'happened_at' => '1968',
@@ -121,7 +121,7 @@ class HeritageTableSeeder extends Seeder
         
         $heritage = Heritage::create(
         	[
-        	'asset_name' => 'Koning1.jpg',
+        	'asset_name' => '13.jpg',
         	'asset_type' => 'image',
         	'description' => 'Bezoek Koning Boudewijn en Koningin Fabiola',
         	'happened_at' => '3 juli 1964',
@@ -130,7 +130,7 @@ class HeritageTableSeeder extends Seeder
         
         $heritage = Heritage::create(
         	[
-        	'asset_name' => 'Koning2.jpg',
+        	'asset_name' => '14.jpg',
         	'asset_type' => 'image',
         	'description' => 'Koning Boudewijn en koningin Fabiola op bezoek in Roeselare. Koning Boudewijn en koningin Fabiola begroeten het volk op de Grote Markt. In het midden staat ondercommissaris Maurits Seurinck. De dame met het fototoestel is Lucie Cracco.',
         	'happened_at' => '',
@@ -139,7 +139,7 @@ class HeritageTableSeeder extends Seeder
         
         $heritage = Heritage::create(
         	[
-        	'asset_name' => 'Koning3.jpg',
+        	'asset_name' => '15.jpg',
         	'asset_type' => 'image',
         	'description' => 'Carosserie Desot gelegen langs de Bruggesteenweg te Gits kreeg koningklijk bezoek en mocht Koning Bouwdewijn ontvangen tijdens een bedrijfsbezoek te Gits in 1990. Roger Storme, een werknemer van het bedrijf, kreeg de eer om de koning te ontvangen en hem een boek over het bedrijf te overhandigen.',
         	'happened_at' => '1990',
@@ -148,7 +148,7 @@ class HeritageTableSeeder extends Seeder
         
         $heritage = Heritage::create(
         	[
-        	'asset_name' => 'Gebouwen2.jpg',
+        	'asset_name' => '16.jpg',
         	'asset_type' => 'image',
         	'description' => 'Godshuis Lichtervelde. Doordat tijdens de Eerste Wereldoorlog de klaslokalen werden opgeëist door de Duitsers, kregen de jongens in Lichtervelde les in kamertjes van het godshuis.',
         	'happened_at' => '1968',
@@ -157,7 +157,7 @@ class HeritageTableSeeder extends Seeder
         
         $heritage = Heritage::create(
         	[
-        	'asset_name' => 'Gebouwen3.jpg',
+        	'asset_name' => '17.jpg',
         	'asset_type' => 'image',
         	'description' => 'De foto geeft een mooi beeld van de Markt met kerk in Moorslede in februari 1917.  Rechts op de achtergrond staat het gemeentehuis van tijdens de Eerste Wereldoorlog (wit gebouw met torentje).',
         	'happened_at' => 'februari 1917',
@@ -166,7 +166,7 @@ class HeritageTableSeeder extends Seeder
         
         $heritage = Heritage::create(
         	[
-        	'asset_name' => 'Gebouwen1.jpg',
+        	'asset_name' => '18.jpg',
         	'asset_type' => 'image',
         	'description' => 'Postkaart van de Hofmolen te Lichtervelde uitgegeven door Sintobin-Yperman.  Deze molen was wellicht de oudste van Lichtervelde en verdween toen hij helemaal op het einde van de Eerste Wereldoorlog opgeblazen werd door de wegtrekkende Duitse troepen.  De laatste uitbater was Boutte, vandaar dat men ook spreekt van Bouttens molen.',
         	'happened_at' => '1954',
@@ -175,7 +175,7 @@ class HeritageTableSeeder extends Seeder
 
         $heritage = Heritage::create(
         	[
-        	'asset_name' => 'Personen1.jpg',
+        	'asset_name' => '19.jpg',
         	'asset_type' => 'image',
         	'description' => 'Jules Mestdagh, geboren 1 oktober 1894, werd soldaat in Lier. Daar kreeg hij opleiding bij de Grenadiers 1ste bataljon, vierde compagnie. Daar is hij bijgebleven voor zijn hele legerdienst. Hij kreeg hiervoor een erediploma van de gemeente Ingelmunster.',
         	'happened_at' => '1914-1918',
@@ -183,7 +183,7 @@ class HeritageTableSeeder extends Seeder
         $heritage->categories()->sync([8]);
         	
         $heritage = Heritage::create([
-        	'asset_name' => 'Personen2.jpg',
+        	'asset_name' => '20.jpg',
         	'asset_type' => 'image',
         	'description' => 'Portretfoto van Ortskommandant major Prasse van Roeselare. ',
         	'happened_at' => '1914-1918',
@@ -191,7 +191,7 @@ class HeritageTableSeeder extends Seeder
         $heritage->categories()->sync([8,9]);
 
         $heritage = Heritage::create([
-            'asset_name' => 'nutella.jpg',
+            'asset_name' => '21.jpg',
             'asset_type' => 'image',
             'description' => 'Pietro Ferrero maakte een pasta bestaande uit hazelnoten, suiker en een snuifje cacao. Dit werd uitgebracht onder de naam SuperCrema, nu beter bekend als Nutella.',
             'happened_at' => '14 mei 1946',
@@ -199,7 +199,7 @@ class HeritageTableSeeder extends Seeder
         $heritage->categories()->sync([1,10]);
 
         $heritage = Heritage::create([
-            'asset_name' => 'mcdonalds.jpg',
+            'asset_name' => '22.jpg',
             'asset_type' => 'image',
             'description' => 'Het eerste McDonald fastfoodketen restaurant werd geopend naast de Monrovia luchthaven in LA, US',
             'happened_at' => '1937',
@@ -207,7 +207,7 @@ class HeritageTableSeeder extends Seeder
         $heritage->categories()->sync([10]);
 
         $heritage = Heritage::create([
-            'asset_name' => 'borstbollen.jpg',
+            'asset_name' => '23.jpg',
             'asset_type' => 'image',
             'description' => 'Camille Wyckmans maakte de eerste borstbollen op basis van een regionaal recept. Aanvankelijk verkocht hij ze enkel thuis in zijn eigen kruidenierswinkel maar dit werd vlug uitgebreid naar grotere locaties.',
             'happened_at' => '1946',
