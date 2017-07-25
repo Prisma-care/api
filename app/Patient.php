232c5524ac17107fda35131768db58804591268f
Michiel Leyman <michiel.leyman@gmail.com>
2017-07-25 08:30:23 +0200

    Prepopulate albums from certain categories

--- app/Patient.php
+++ app/Patient.php
@@ -2,8 +2,13 @@
 
 namespace App;
 
+use App\Album;
+use App\Story;
+use App\Category;
+use App\Heritage;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Collection;
 
 class Patient extends Model
 {
@@ -31,5 +36,40 @@ class Patient extends Model
     public function users()
     {
         return $this->belongsToMany('App\User');
+    }
+
+    /**
+     * Premake albums specified in $categories, fill them up with heritage data.
+     * Ties into the FTUE, a patient should never have an empty story.
+     *
+     * @return void
+     */
+    public function prepopulate()
+    {
+        $categories = ['Sport', 'Voeding', 'Roeselare'];
+        $ids = [];
+        foreach ($categories as $category) {
+            $category = Category::where('name', '=', $category)->first();
+            $album = Album::create([
+                'title' => $category->name,
+                'patient_id' => $this->id
+            ]);
+            $heritage = $category->heritage;
+            $this->createStoriesFromHeritage($heritage, $album->id);
+        }
+    }
+
+    private function createStoriesFromHeritage(Collection $heritageData, int $albumId)
+    {
+        foreach ($heritageData as $heritage) {
+            $story = Story::create([
+                'description' => ($heritage->description) ?: "",
+                'asset_name' => $heritage->asset_name,
+                'asset_type' > $heritage->asset_type,
+                // TODO The user id should be user named Prisma or System
+                'user_id' => 1,
+                'album_id' => $albumId
+            ]);
+        }
     }
 }
