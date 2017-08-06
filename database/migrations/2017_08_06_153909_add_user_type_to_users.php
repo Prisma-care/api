f7977fd7ea119b456bdd25aa0fa5ec588548a156
Gilbert West <gilbertwest@gmail.com>
2017-08-06 17:47:31 +0200

    Migration to add the user_type to the users table

---
+++ database/migrations/2017_08_06_153909_add_user_type_to_users.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserTypeToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->string('user_type',50)->default('family');
+        });
+    }
+}
