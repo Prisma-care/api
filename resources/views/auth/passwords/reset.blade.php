0c2226ead06e92fd0d27c566a90e08cd7018526d
Gilbert West <gilbertwest@gmail.com>
2017-08-08 00:46:35 +0200

    Set password headings

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
+                <div class="panel-heading">Set Password</div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -63,7 +63,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Reset Password
+                                    Set Password
                                 </button>
                             </div>
                         </div>
