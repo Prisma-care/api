df6dc3ab3972d7c9fc318d490578af1ddb35ee9a
Gilbert West <gilbertwest@gmail.com>
2017-08-10 18:17:31 +0200

    start email blade

---
+++ resources/views/emails/invite.blade.php
@@ -0,0 +1,14 @@
+/*
+
+TO DO: Move to the mailable
+Subject: {{ $inviter }} nodigde je uit voor Prisma
+*/
+
+Geef mee kleur aan de herinneringen van Marie-Josée met de Prisma app voor Android smartphones en tablets.
+
+[[Bevestig je e-mail adres via deze link]] en kies daarna een wachtwoord.
+
+Werkt de link niet? Plak deze link in je internet browser: $link
+
+Meer weten over Prisma?
+www.prisma.care
