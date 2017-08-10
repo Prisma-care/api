45cb0c7b592938432e0160e9ca00a63e18a56ccf
Gilbert West <gilbertwest@gmail.com>
2017-08-10 18:11:09 +0200

    Remove password confirmation

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -19,7 +19,6 @@
 
                         <input type="hidden" name="token" value="{{ $token }}">
 
-                        <p>Thank you for accepting the invitation to Prisma.care</p>
                         <p>To activate your account please set your password below. We'll then send you to the page where you can download the Prisma app to your Android phone.</p>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
@@ -39,11 +38,8 @@
                         <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
                             <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
 
-                                @if ($errors->has('password_confirmation'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
                                     </span>
                                 @endif
                             </div>
