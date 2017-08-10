0352e5d362ffd5eec96c02e572db855a54c57df8
Gilbert West <gilbertwest@gmail.com>
2017-08-10 18:44:22 +0200

    New confirmation text and a wee code format

--- resources/views/invite-confirmation.blade.php
+++ resources/views/invite-confirmation.blade.php
@@ -1,25 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Installeer de Prisma app</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Installeer de Prisma app</div>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="panel-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                    <a href="#" class="btn btn-primary">Download nu in de Google Play store</a>
-                        <p>Na het installeren en openen van de app dien je je e-mail adres en wachtwoord nog in te geven. Hou deze goed bij.</p>
+                        <p>Je hebt nu een account voor de Prisma app.</p>
 
+                        <a href="#" class="btn btn-primary">Installeer de app</a> en meldt je aan.
+
+                        <p>Na het installeren en openen van de app dien je je e-mail adres en wachtwoord nog in te
+                            geven. Hou deze goed bij.</p>
+
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
