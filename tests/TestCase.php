0fde5c2ce3b15ccafec064204faaa60f63c16116
Michiel Leyman <MichielLeyman@users.noreply.github.com>
2017-08-12 17:03:56 +0200

    Use factories for test database seeding (#97)
    * Remove TestDatabase seeders
    
    * Create initial factory
    
    * Use factories for Patient creation
    
    * Use imports
    
    * Set up remaining defaults
    
    * Seed database with factories in setup

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,9 @@ namespace Tests;
 
 use JWTAuth;
 use App\User;
+use App\Patient;
+use App\Album;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
@@ -26,7 +29,8 @@ abstract class TestCase extends BaseTestCase
     {
         parent::setUp();
         $this->artisan('migrate:refresh');
-        $this->artisan('db:seed', ['--class' => 'TestDatabaseSeeder']);
+        $this->artisan('db:seed');
+        $this->seedDatabase();
     }
 
     protected function authenticate($user = null)
@@ -37,6 +41,43 @@ abstract class TestCase extends BaseTestCase
         $token = JWTAuth::fromUser($user);
         $this->headers['HTTP_Authorization'] = 'Bearer ' . $token;
         $this->refreshApplication();
+    }
+
+    public function seedDatabase()
+    {
+        $this->seedDefaults();
+        $this->seedUsers();
+        $this->seedPatients();
+    }
+
+    private function seedDefaults()
+    {
+        $user = factory(User::class)->create([
+            'email' => 'testing@prisma.care',
+            'password' => Hash::make('testing@prisma.care')
+        ]);
+        $patient = factory(Patient::class)->create([
+            'first_name' => 'Patient',
+            'last_name' => 'Testing'
+        ]);
+        $patient->prepopulate();
+        $album = factory(Album::class)->create([
+            'title' => 'Taken',
+            'patient_id' => $patient->id
+        ]);
+    }
+
+    public function seedUsers($amount = 5)
+    {
+        factory(User::class, $amount)->create();
+    }
+
+    public function seedPatients($amount = 5)
+    {
+        $patients = factory(Patient::class, $amount)->create();
+        foreach ($patients as $patient) {
+            $patient->prepopulate();
+        }
     }
 
     /**
