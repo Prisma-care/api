508895fd41a1cd949b97d278f083480b9fdf1324
Gilbert West <gilbertwest@gmail.com>
2017-08-12 16:19:07 +0200

    Pass the user to the contructor

--- app/Mail/Invitation.php
+++ app/Mail/Invitation.php
@@ -11,14 +11,16 @@ class Invitation extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public $user;
+
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($user)
     {
-        //
+        $this->user = $user;
     }
 
     /**
