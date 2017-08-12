a9ff1a276a46e52f06f9c69283dbb88716d1391c
Gilbert West <gilbertwest@gmail.com>
2017-08-12 10:33:28 +0200

    Revert to the basic reset password page

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Set Password</div>
+                <div class="panel-heading">Reset Password</div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -19,14 +19,11 @@
 
                         <input type="hidden" name="token" value="{{ $token }}">
 
-                        <h2>Welkom bij Prisma</h2>
-                        <p> Kies een wachtwoord dat je makkelijk kan onthouden want je hebt het straks nodig bij het starten van de app.</p>
-
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">E-mail</label>
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" required>
+                                <input id="email" type="email" class="form-control" name="email" value="{{ $email or old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
@@ -37,7 +34,7 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Wachtwoord</label>
+                            <label for="password" class="col-md-4 control-label">Password</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password" required>
@@ -50,9 +47,24 @@
                             </div>
                         </div>
 
+                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+
+                                @if ($errors->has('password_confirmation'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">Aanmelden</button>
+                                <button type="submit" class="btn btn-primary">
+                                    Reset Password
+                                </button>
                             </div>
                         </div>
                     </form>
