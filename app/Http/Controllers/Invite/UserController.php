a9bd2799fc3cb5b8bba89a83afd26d433aa53f36
Gilbert West <gilbertwest@gmail.com>
2017-08-12 10:46:48 +0200

    UserController for invites

---
+++ app/Http/Controllers/Invite/UserController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Validator;
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+            'firstName' => 'required',
+            'lastName' => 'required',
+            'patientID' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->exception($validator->errors(), 400);
+        }
+
+        $user_data = [
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+            'first_name' => $request->input('firstName'),
+            'last_name' => $request->input('lastName')
+        ];
+
+        $user = User::create($user_data);
+
+        if (!$user->save()) {
+            return response()->exception('Unexpected error while creating the user', 500);
+        }
+
+        // TO DO : connect the user to the patient
+        // TO DO : issue and email invite
+
+        $createdUser = [
+            'id' => $user->id,
+            'email' => $user->email
+        ];
+
+        $location = $request->url() . '/' . $user->id;
+        return response()->success($createdUser, 201, 'Created', $location);
+    }
+}
