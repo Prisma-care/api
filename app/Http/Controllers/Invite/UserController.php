3955358bd702189ac5949e4ee5878a3e63a65c08
Gilbert West <gilbertwest@gmail.com>
2017-08-12 10:47:06 +0200

    Use short form of hash

--- app/Http/Controllers/Invite/UserController.php
+++ app/Http/Controllers/Invite/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Validator;
 use App\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
+use Hash;
 
 class UserController extends Controller
 {
