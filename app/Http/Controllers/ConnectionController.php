e8644ee3ae123079686699b27a9d3183732789e5
Gilbert West <gilbertwest@gmail.com>
2017-08-17 12:27:28 +0200

    Remove imports

--- app/Http/Controllers/ConnectionController.php
+++ app/Http/Controllers/ConnectionController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use JWTAuth;
 use App\Patient;
-use Illuminate\Http\Request;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class ConnectionController extends Controller
 {
