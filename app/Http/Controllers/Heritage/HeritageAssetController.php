15134616063f31d77422bcb14cdec3a8da2468d2
Michiel Leyman <michiel.leyman@gmail.com>
2017-08-18 20:44:46 +0200

    Add Show HeritageAsset

--- app/Http/Controllers/Heritage/HeritageAssetController.php
+++ app/Http/Controllers/Heritage/HeritageAssetController.php
@@ -27,11 +27,24 @@ class HeritageAssetController extends Controller
      * Display the specified resource.
      *
      * @param  \App\Http\Requests\HeritageAsset\Show $request
+     * @param  int  $heritageId
      * @param  int  $assetId
      * @return \Illuminate\Http\Response
      */
-    public function show(HeritageAssetRequest\Show $request, $assetId)
+    public function show(HeritageAssetRequest\Show $request, $heritageId, $assetId)
     {
+        Heritage::findOrFail($heritageId);
+
+        $storagePath = storage_path("app/heritage/$heritageId/$asset");
+
+        if (!File::exists($storagePath)) {
+            return response()->exception('This asset does not exist.', 404);
+        }
+
+        $file = File::get($storagePath);
+        $mimeType = File::mimeType($storagePath);
+
+        return response($file, 200)->header("Content-Type", $mimeType);
         return response()->success([], 204, 'No Content');
     }
 }
