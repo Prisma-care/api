948013c8aeecb037b0b6d151f4e55a57e3611276
Gilbert West <gilbertwest@gmail.com>
2017-08-20 18:35:50 +0200

    Unser Controller for handing the creation of new user and sending invite

--- app/Http/Controllers/Invite/UserController.php
+++ app/Http/Controllers/Invite/UserController.php
@@ -3,15 +3,14 @@
 namespace App\Http\Controllers\Invite;
 
 use App\Http\Controllers\Controller;
+use App\Invite;
 use App\Mail\Invitation;
 use App\Patient;
 use App\User;
-use Hash;
 use Mail;
 
 class UserController extends Controller
 {
-
     /**
      * @param StoreUserConnection $request
      * @return mixed
@@ -26,18 +25,40 @@ class UserController extends Controller
             'last_name' => $request->input('lastName')
         ];
 
+        $patientId = $request->input('patientId');
+        $inviterId = $request->input('inviterId');
+
         $user = User::create($user_data);
 
         if (!$user->save()) {
             return response()->exception('Unexpected error while creating the user', 500);
         }
 
-        app('auth.password.tokens')->create($user);
+        $inviter = User::find($inviterID)->full_name;
 
-        $patient = Patient::findOrFail($request->input($patientId));
+        $patient = Patient::findOrFail($patientId);
         $patient->users()->attach($user->id);
+        $patient_name = $patient->full_name;
 
-        Mail::to($user)->send(new Invitation($patient));
+        $token = str_random(40);
+
+        $invite = [
+            'email' => $user->email,
+            'user_id' => $user->id,
+            'token' => $token,
+            'patient_id' => $patientId,
+            'inviter_id' => $inviterId
+        ];
+
+        Invite::create($invite);
+
+        $data = [
+            'patient' => $patient_name,
+            'inviter' => $inviter,
+            'token' => $token
+        ];
+
+        Mail::to($user)->send(new Invitation($data));
 
         return response()->success('Invitation email sent', 204);
     }
