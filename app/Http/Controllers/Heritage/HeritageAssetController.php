48762a2d6656740ad9117594cc3c594981ff0e2b
Michiel Leyman <michiel.leyman@gmail.com>
2017-09-01 17:33:31 +0200

    Allow heritage youtube asset storing

--- app/Http/Controllers/Heritage/HeritageAssetController.php
+++ app/Http/Controllers/Heritage/HeritageAssetController.php
@@ -15,6 +15,23 @@ class HeritageAssetController extends Controller
         $this->middleware('jwt.auth');
     }
 
+    private function attachImageAsset($request, $heritage)
+    {
+        $asset = $request->file('asset');
+        $extension = ($asset->extension()) ? ($asset->extension()) : pathinfo($asset, PATHINFO_EXTENSION);
+
+        $assetName = $heritage->id;
+        $fullAssetName = "$assetName.$extension";
+        $storagePath = "heritage/$heritage->id";
+        $asset->storeAs($storagePath, $fullAssetName);
+        ImageUtility::saveThumbs($asset, $storagePath, $assetName, $extension);
+
+        $location = $request->url() . '/' . $fullAssetName;
+        $heritage->asset_name = $location;
+        $heritage->asset_type = 'image';
+        $heritage->save();
+    }
+
     /**
      * Store a newly created resource in storage.
      *
@@ -25,26 +42,25 @@ class HeritageAssetController extends Controller
     public function store(HeritageAssetRequest\Store $request, $albumId, $heritageId)
     {
         $heritage = Heritage::findOrFail($heritageId);
+        $assetType = $request->input('assetType');
+        if (!$assetType || $assetType === 'image') {
+            if (!$request->hasFile('asset')) {
+                return response()->exception('No asset was provided or the form-data request was malformed', 400);
+            } elseif (!$request->file('asset')->isValid()) {
+                return response()->exception('Asset upload failed, please try again later.', 500);
+            }
 
-        if (!$request->hasFile('asset')) {
-            return response()->exception('No asset was provided or the form-data request was malformed', 400);
-        } elseif (!$request->file('asset')->isValid()) {
-            return response()->exception('Asset upload failed, please try again later.', 500);
+            $this->attachImageAsset($request, $heritage);
+            $location = $heritage->asset_name;
+            return response()->success(['id'=> $heritage->id], 201, 'Created', $location);
+        } elseif ($assetType === 'youtube') {
+            $heritage->asset_name = $request->input('asset');
+            $heritage->asset_type = 'youtube';
+            $heritage->save();
+
+            $location = $request->url() . '/' . $heritage->id;
+            return response()->success(['id'=> $heritage->id], 201, 'Created', $location);
         }
-
-        $asset = $request->file('asset');
-        $extension = ($asset->extension()) ? ($asset->extension()) : pathinfo($asset, PATHINFO_EXTENSION);
-
-        $assetName = $heritageId;
-        $fullAssetName = "$assetName.$extension";
-        $storagePath = "heritage/$heritageId";
-        $asset->storeAs($storagePath, $fullAssetName);
-        ImageUtility::saveThumbs($asset, $storagePath, $assetName, $extension);
-
-        $location = $request->url() . '/' . $fullAssetName;
-        $heritage->asset_name = $location;
-        $heritage->save();
-        return response()->success(['id'=> $heritage->id], 201, 'Created', $location);
     }
 
     /**
