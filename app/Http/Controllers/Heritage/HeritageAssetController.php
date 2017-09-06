4cca9c3b309230382ec6320bc1d7821735013a8f
Gilbert West <gilbertwest@gmail.com>
2017-09-06 18:08:06 +0200

    Heritage Asset Docblock

--- app/Http/Controllers/Heritage/HeritageAssetController.php
+++ app/Http/Controllers/Heritage/HeritageAssetController.php
@@ -13,6 +13,8 @@ use App\Http\Requests\HeritageAsset as HeritageAssetRequest;
  * @package App\Http\Controllers\Heritage
  * @resource Heritage\HeritageAsset
  *
+ * HeritageAssets are photographic and video materials used to stimulate discussion between the User and Patient
+ * HeritageAssets are supplied by local heritage organisations rather than from the Users
  */
 
 class HeritageAssetController extends Controller
@@ -45,6 +47,10 @@ class HeritageAssetController extends Controller
 
 
     /**
+     * Store a new HeritageAsset and attach it to a Heritage
+     *
+     * HeritageAssets can be photos or URLs to videos on external services such as YouTube or Vimeo
+     *
      * @param HeritageAssetRequest\Store $request
      * @param $albumId
      * @param $heritageId
@@ -80,6 +86,8 @@ class HeritageAssetController extends Controller
 
 
     /**
+     * Fetch a particular HeritageAsset
+     *
      * @param HeritageAssetRequest\Show $request
      * @param $albumId
      * @param $heritageId
