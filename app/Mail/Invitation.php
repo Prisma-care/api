5303153b9df1c893125a606e38f2b65f29ed057b
Gilbert West <gilbertwest@gmail.com>
2017-09-06 14:34:00 +0200

    Update DocBlocks

--- app/Mail/Invitation.php
+++ app/Mail/Invitation.php
@@ -15,11 +15,12 @@ class Invitation extends Mailable
 
     public $data;
 
+
     /**
-     * Create a new message instance.
-     *
-     * @return void
+     * Invitation constructor. Create a new message instance.
+     * @param $data
      */
+
     public function __construct($data)
     {
         $this->data = $data;
