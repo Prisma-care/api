066c88425859a489608c06e8e9a7442620db4d9b
Michiel Leyman <michiel.leyman@gmail.com>
2017-09-10 17:41:16 +0200

    Fake local disk setup

--- tests/Feature/StoryAssetTest.php
+++ tests/Feature/StoryAssetTest.php
@@ -5,15 +5,19 @@ namespace Tests\Feature;
 use App\Story;
 use App\Patient;
 use Tests\TestCase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class StoryAssetTest extends TestCase
 {
-    private $ownedStoryId;
     private $baseEndpoint = 'v1/patient/{patientId}/story/{storyId}/asset';
+    private $diskName = 'story-asset-tests';
     private $endpoint;
+    private $ownedStoryId;
+    private $storagePath;
 
     private function getPopulatedEndpoint($patientId = null, $storyId = null)
     {
@@ -25,10 +29,13 @@ class StoryAssetTest extends TestCase
     {
         parent::setUp();
         $this->authenticate();
-        $this->ownedStoryId = Patient::find($this->testPatientId)
-                                ->albums()->get()->values()->first()
-                                ->stories()->first()->id;
+        $patient = Patient::find($this->testPatientId);
+        $this->ownedStoryId = $patient->albums()->get()->values()->first()->stories()->first()->id;
         $this->endpoint = $this->getPopulatedEndpoint();
+
+        Storage::fake($this->diskName);
+        $this->storagePath = "$patient->id/$this->ownedStoryId";
+        UploadedFile::fake()->image("$this->storagePath/$this->ownedStoryId.jpg");
     }
 
     public function testResourceIsProtected()
