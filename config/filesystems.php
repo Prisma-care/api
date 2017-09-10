cad19a4b43058276ecfecc7952560bac0451e1d6
Michiel Leyman <michiel.leyman@gmail.com>
2017-09-10 20:35:24 +0200

    Set testing filesystem

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,12 @@ return [
             'visibility' => 'public',
         ],
 
+        'testing' => [
+            'driver' => 'local',
+            'root' => storage_path('framework/testing/disks'),
+            'visibility' => 'public'
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_KEY'),
