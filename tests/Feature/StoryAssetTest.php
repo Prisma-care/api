1beaf9cf43850503b21dcec0b5da37a9923466ee
Michiel Leyman <michiel.leyman@gmail.com>
2017-09-10 23:23:57 +0200

    Test GET of predefined asset

--- tests/Feature/StoryAssetTest.php
+++ tests/Feature/StoryAssetTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Story;
 use App\Patient;
 use Tests\TestCase;
+use Tests\Utils\Image;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
@@ -13,16 +14,27 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class StoryAssetTest extends TestCase
 {
-    private $baseEndpoint = 'v1/patient/{patientId}/story/{storyId}/asset';
-    private $diskName = 'story-asset-tests';
-    private $endpoint;
-    private $ownedStoryId;
+    private $diskName = 'stories';
     private $storagePath;
+    private $pathToStaticImage;
+
+    private $baseEndpoint = 'v1/patient/{patientId}/story/{storyId}/asset';
+    private $endpoint;
+    private $specificEndpoint;
+
+    private $ownedStoryId;
 
     private function getPopulatedEndpoint($patientId = null, $storyId = null)
     {
         $tmpEndpoint = str_replace('{patientId}', $patientId ?: $this->testPatientId, $this->baseEndpoint);
         return str_replace('{storyId}', $storyId ?: $this->ownedStoryId, $tmpEndpoint);
+    }
+
+    private function setUpStorage()
+    {
+        Storage::fake($this->diskName);
+        $this->storagePath = Storage::disk($this->diskName)
+                                ->getDriver()->getAdapter()->getPathPrefix();
     }
 
     public function setUp()
@@ -33,9 +45,13 @@ class StoryAssetTest extends TestCase
         $this->ownedStoryId = $patient->albums()->get()->values()->first()->stories()->first()->id;
         $this->endpoint = $this->getPopulatedEndpoint();
 
-        Storage::fake($this->diskName);
-        $this->storagePath = "$patient->id/$this->ownedStoryId";
-        UploadedFile::fake()->image("$this->storagePath/$this->ownedStoryId.jpg");
+        $this->setUpStorage();
+        $path = "$patient->id/$this->ownedStoryId/$this->ownedStoryId.jpg";
+        $fullPath = $this->storagePath  . $path;
+        Storage::disk($this->diskName)->makeDirectory("$patient->id/$this->ownedStoryId");
+        Image::generate($fullPath);
+
+        $this->specificEndpoint = "$this->endpoint/$this->ownedStoryId.jpg";
     }
 
     public function testResourceIsProtected()
@@ -44,5 +60,15 @@ class StoryAssetTest extends TestCase
         unset($headers['HTTP_Authorization']);
         $response = $this->postJson($this->endpoint, [], $headers)
             ->assertStatus(401);
+    }
+
+    public function testGetStoryAsset($location = null)
+    {
+        $endpoint = $this->specificEndpoint;
+        if ($location) {
+            $endpoint = $this->parseResourceLocation($location);
+        }
+        $response = $this->getJson($endpoint, $this->headers)
+            ->assertStatus(200);
     }
 }
