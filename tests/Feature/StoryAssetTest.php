6b9f5dd83784ee8a02b55d59441000d524fec721
Michiel Leyman <michiel.leyman@gmail.com>
2017-09-10 17:17:44 +0200

    Do StoryAssetTest setup

--- tests/Feature/StoryAssetTest.php
+++ tests/Feature/StoryAssetTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Story;
+use App\Patient;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -10,6 +11,26 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class StoryAssetTest extends TestCase
 {
+    private $ownedStoryId;
+    private $baseEndpoint = 'v1/patient/{patientId}/story/{storyId}/asset';
+    private $endpoint;
+
+    private function getPopulatedEndpoint($patientId = null, $storyId = null)
+    {
+        $tmpEndpoint = str_replace('{patientId}', $patientId ?: $this->testPatientId, $this->baseEndpoint);
+        return str_replace('{storyId}', $storyId ?: $this->ownedStoryId, $tmpEndpoint);
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->authenticate();
+        $this->ownedStoryId = Patient::find($this->testPatientId)
+                                ->albums()->get()->values()->first()
+                                ->stories()->first()->id;
+        $this->endpoint = $this->getPopulatedEndpoint();
+    }
+
     public function testPlaceholder()
     {
         $this->assertEquals(true, true);
