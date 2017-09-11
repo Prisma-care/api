e1acb58d949992e5c2a923aa17655673157a2f22
Michiel Leyman <michiel.leyman@gmail.com>
2017-09-11 21:33:35 +0200

    Default album test setup

---
+++ tests/Feature/DefaultAlbumTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Album;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class DefaultAlbumTest extends TestCase
+{
+    private $endpoint = 'v1/album';
+    private $specificEndpoint;
+    private $baseObject = [
+        'id' => null,
+        'title' => 'Sports',
+        'heritage' => [
+            [
+              'id' => 1,
+              'description' => 'A description',
+              'asset_type' => 'image',
+              'asset_name' => null,
+              'favorited' => false,
+              'album_id' => 0
+            ]
+        ]
+    ];
+
+    private $baseObjectStructure;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->authenticate();
+        $this->baseObjectStructure = array_merge(
+            array_keys($this->baseObject),
+            [
+              'heritage' => [
+                '*' => array_keys($this->baseObject['heritage'][0])
+              ]
+            ]
+        );
+        $defaultAlbum = Album::with('heritage')->get()
+                            ->where('patient_id', '=', null)->values()->first();
+        $this->specificEndpoint = "$this->endpoint/$defaultAlbum->id";
+    }
+
+    public function testPlaceholder()
+    {
+        $this->assertEquals(true, true);
+    }
+}
