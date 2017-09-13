7560df2bd09612046e285b5ce6e681bcab563a45
Michiel Leyman <michiel.leyman@gmail.com>
2017-09-13 20:29:26 +0200

    Heritage test setup

---
+++ tests/Feature/HeritageTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Story;
+use App\Album;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class HeritageTest extends TestCase
+{
+    private $baseEndpoint = 'v1/album/{albumId}/story';
+    private $endpoint;
+    private $specificEndpoint;
+
+    private $defaultAlbumId;
+    private $testHeritageId;
+    private $baseObject = [
+        'id' => null,
+        'description' => 'A description',
+        'happened_at' => null,
+        'album_id' => 1,
+        'asset_name' => null,
+        'asset_type' => 'image'
+    ];
+
+    private function getEndpointWithAlbumId($albumId = null)
+    {
+        return str_replace('{albumId}', $albumId ?: $this->testPatientId, $this->baseEndpoint);
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->authenticate();
+        $defaultAlbum = Album::with('heritage')->get()
+                            ->where('patient_id', '=', null)->values()->first();
+        $this->defaultAlbumId = $defaultAlbum->id;
+        $this->testHeritageId = $defaultAlbum->heritage()->first()->id;
+        $this->baseObject['album_id'] = $this->defaultAlbumId;
+        $this->endpoint = $this->getEndpointWithAlbumId();
+        $this->specificEndpoint = "$this->endpoint/$this->testHeritageId";
+    }
+}
