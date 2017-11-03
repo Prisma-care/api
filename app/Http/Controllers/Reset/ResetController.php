c2e6fdfb382c60ff1ebd598d38ac9e42c643d61d
Gilbert West <gilbertwest@gmail.com>
2017-11-03 17:23:12 +0100

    Password reset controller

---
+++ app/Http/Controllers/Reset/ResetController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers\Reset;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Reset\StoreReset;
+use App\Mail\SendReset;
+use App\Mail\SendNewPassword;
+use App\User;
+use Carbon\Carbon;
+use Hash;
+use Mail;
+use View;
+
+class ResetController extends Controller
+{
+
+    /**
+     * Create a new controller instance.
+     *
+     * No explicit return
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+
+    /**
+     * @param StoreReset $request
+     */
+    public function store(StoreReset $request){
+
+        $email = $request->email;
+        $token = str_random(40);
+        $created_at = Carbon::now();
+
+        DB::table('password_resets')->insert(
+            ['email' => $email, 'token' => $token, 'created_at' => $created_at]
+        );
+
+        // mail them a tokenized link
+    }
+
+    /**
+     * Check reset token
+     *
+     * Check for a valid token and show the update password form if valid
+     *
+     * @param string $token
+     * @return \Illuminate\Contracts\View\View
+     */
+    public function checkToken(string $token)
+    {
+        $reset = DB::table('password_resets')->where('token', $token)->first();
+
+        if(!$reset) { abort(404,'Token has already been used or is invalid'); }
+
+        $data = [
+            'email' => $reset->email,
+            'token' => $reset->token
+        ];
+
+        return View::make('reset.set', $data);
+
+    }
+
+
+    /**
+     * Set new password
+     *
+     * @param SetNewPassword $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function set(SetNewPassword $request)
+    {
+        $email = $request->input('email');
+        $new_password = Hash::make($request->input('password'));
+
+        User::where('email', $email)->update(['password' => $new_password]);
+
+        $this->destroyToken($request->input('token'));
+
+        $data = [
+            'user_name' => $user->first_name,
+            'password' => $new_password
+        ];
+
+        Mail::to($user)->send(new SendNewPassword($data));
+
+        return view('invites.confirmation');
+
+    }
+
+    /**
+     * Destroy Invite Token
+     *
+     * Delete the token used for this reset
+     * @param $token
+     */
+    private function destroyToken($token)
+    {
+        DB::table('password_resets')->where('token', $token)->delete();
+    }
+}
