0d3784735e073afd4479d39b8fc4616a817bdbec
Gilbert West <gilbertwest@gmail.com>
2017-11-09 18:06:12 +0100

    Fix class name clash

--- app/Mail/SendPasswordResetLink.php
+++ app/Mail/SendPasswordResetLink.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class SendPassword extends Mailable
+class SendPasswordResetLink extends Mailable
 {
     use Queueable, SerializesModels;
 
