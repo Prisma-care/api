61d8e639624c358459e342b843d9df95a77bcbf5
Gilbert West <gilbertwest@gmail.com>
2017-11-09 11:54:54 +0100

    Save the login time to user table

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -34,6 +34,9 @@ class LoginController extends Controller
             } else {
                 $userId = Auth::user()->id;
                 $user = User::find($userId);
+                $user->last_login = Carbon::now();
+                $user->save();
+
                 $patients = $user->patients()
                     ->select(['patient_id', 'first_name', 'last_name'])
                     ->get()->values()->all();
