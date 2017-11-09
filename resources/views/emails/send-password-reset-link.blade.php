bd32f1c94583ab8c055bba0e19e8504976f8be51
Gilbert West <gilbertwest@gmail.com>
2017-11-09 16:58:02 +0100

    translation

--- resources/views/emails/send-password-reset-link.blade.php
+++ resources/views/emails/send-password-reset-link.blade.php
@@ -1,6 +1,6 @@
 @component('mail::message')
-    <p>We have received a password reset request. If you did not make this request, please ignore this email.</p>
-    <p><a href="{{ route('reset.check',['token'=>$token]) }}" target="_blank">Please click here to reset your password</a>.</p>
+    <p>Iemand vroeg om je Prisma wachtwoord opnieuw in te stellen. Indien jij dit niet was, dan kan je deze email negeren.</p>
+    <p><a href="{{ route('reset.check',['token'=>$token]) }}" target="_blank">Stel je wachtwoord opnieuw in</a>.</p>
     <p>Werkt de link niet? Plak deze link in je internet browser: {{ route('reset.check',['token'=>$token]) }}</p>
     <p>Meer weten over Prisma?<br>
         www.prisma.care</p>
