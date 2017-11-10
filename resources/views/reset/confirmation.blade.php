14b28b2c65adb80a2515be1f4ea964a27ee20baf
Gilbert West <gilbertwest@gmail.com>
2017-11-10 13:46:52 +0100

    Confirmation page

---
+++ resources/views/reset/confirmation.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Success</h4></div>
+
+                    <div class="panel-body">
+
+                        <p>Het wachtwoord van uw account is gewijzigd.</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
