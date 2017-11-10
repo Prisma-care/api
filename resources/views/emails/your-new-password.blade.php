7df60ee85c106f647b8fdc11f0c28cb9004188e9
Gilbert West <gilbertwest@gmail.com>
2017-11-10 16:10:17 +0100

    New text for password reminder email

--- resources/views/emails/your-new-password.blade.php
+++ resources/views/emails/your-new-password.blade.php
@@ -1,4 +1,6 @@
 @component('mail::message')
 <p>Beste {{ $user_name }}</p>
-<p>Je wachtwoord is {{ $password }}</p>
+<p>Je nieuwe Prisma wachtwoord is <em>{{ $password }}</em>.</p>
+<p>Geniet van leuke momenten met je geliefden.</p>
+<p>Meer info over Prisma vind je op www.prisma.care</p>
 @endcomponent
