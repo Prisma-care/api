22b4a18cc6ac82f8a840ea1ba5bb508f00601412
Gilbert West <gilbertwest@gmail.com>
2017-11-10 15:57:20 +0100

    replace Uw with Je

--- resources/views/emails/newpassword.blade.php
+++ resources/views/emails/newpassword.blade.php
@@ -1,4 +1,4 @@
 @component('mail::message')
 <p>Beste {{ $user_name }}</p>
-<p>Bedankt om deel uit te maken van Prisma. Uw wachtwoord is {{ $password }}</p>
+<p>Bedankt om deel uit te maken van Prisma. Je wachtwoord is {{ $password }}</p>
 @endcomponent
--- resources/views/emails/your-new-password.blade.php
+++ resources/views/emails/your-new-password.blade.php
@@ -1,4 +1,4 @@
 @component('mail::message')
 <p>Beste {{ $user_name }}</p>
-<p>Uw wachtwoord is {{ $password }}</p>
+<p>Je wachtwoord is {{ $password }}</p>
 @endcomponent
