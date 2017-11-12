9cbd8921e2105e41fd24fddce9c9d5b24d44fcae
Gilbert West <gilbertwest@gmail.com>
2017-11-12 17:02:05 +0100

    Link the URL

--- resources/views/emails/your-new-password.blade.php
+++ resources/views/emails/your-new-password.blade.php
@@ -2,5 +2,5 @@
 <p>Beste {{ $user_name }}</p>
 <p>Je nieuwe Prisma wachtwoord is <em>{{ $password }}</em>.</p>
 <p>Geniet van leuke momenten met je geliefden.</p>
-<p>Meer info over Prisma vind je op www.prisma.care</p>
+<p>Meer info over Prisma vind je op <a href="https://www.prisma.care">prisma.care</a></p>
 @endcomponent
