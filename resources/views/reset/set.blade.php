e98ff80b645a524501f1ac115f7525a2b00e4e92
Gilbert West <gilbertwest@gmail.com>
2017-11-12 17:01:32 +0100

    letter casing changes on reset button

--- resources/views/reset/set.blade.php
+++ resources/views/reset/set.blade.php
@@ -42,8 +42,7 @@
                             <div class="form-group">
                                 <div class="col-md-6 col-md-offset-4">
 
-                                    <button type="submit" class="btn btn-primary">Je Wachtwoord Bijwerken</button> <!-- Update your password -->
-
+                                    <button type="submit" class="btn btn-primary">Je wachtwoord bijwerken</button> <!-- Update your password -->
                                 </div>
                             </div>
                         </form>
