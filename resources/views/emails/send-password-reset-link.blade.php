9546a944d2100562a563af9e10f7d877c29e5985
Gilbert West <gilbertwest@gmail.com>
2017-11-12 17:02:14 +0100

    Link the URL

--- resources/views/emails/send-password-reset-link.blade.php
+++ resources/views/emails/send-password-reset-link.blade.php
@@ -2,5 +2,5 @@
 <p>Iemand vroeg om je Prisma wachtwoord opnieuw in te stellen. Indien jij dit niet was, dan kan je deze email negeren.</p>
 <p><a href="{{ route('reset.check',['token'=>$token]) }}" target="_blank">Stel je wachtwoord opnieuw in</a>.</p>
 <p>Werkt de link niet? Plak deze link in je internet browser: {{ route('reset.check',['token'=>$token]) }}</p>
-<p>Meer weten over Prisma?<br>www.prisma.care</p>
+<p>Meer weten over Prisma?<br><a href="https://www.prisma.care">prisma.care</a></p>
 @endcomponent
