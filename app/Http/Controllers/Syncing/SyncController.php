c3b7f45ed8585ad232b3dd4ede33ff30564ea265
Gilbert West <gilbertwest@gmail.com>
2017-11-29 11:59:33 +0100

    Clear out unneeded boiler plate and create the method to check for any valid sync jobs

---
+++ app/Http/Controllers/Syncing/SyncController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Syncing;
+
+use App\Http\Controllers\Controller;
+use App\Sync;
+use Illuminate\Http\Request;
+
+class SyncController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function checkForSyncs()
+    {
+        try {
+
+            $sync = Sync::whereIn('status', ['ready', 'running'])->findOrFail();
+
+        } catch (Illuminate\Database\Eloquent\ModelNotFoundException $e) {
+
+            // there are no sync jobs to run
+
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Sync $sync
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Sync $sync)
+    {
+        //
+    }
+
+}
