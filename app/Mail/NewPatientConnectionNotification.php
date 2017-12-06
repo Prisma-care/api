48c58bece159301e100e3a7774ed776a7d9eb975
Gilbert West <gilbertwest@gmail.com>
2017-12-06 23:00:34 +0100

    New mailable to notifications to existing users

---
+++ app/Mail/NewPatientConnectionNotification.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Mail;
+
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class NewPatientConnectionNotification extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        $data = $this->data;
+        return $this->from('info@prisma.care', 'Prisma')
+            ->subject($data['inviter']. ' nodigde je uit voor Prisma')
+            ->with($data)
+            ->markdown('emails.new-connection');
+    }
+}
