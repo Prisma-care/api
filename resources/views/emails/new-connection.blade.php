cb496135b8a8ba1f8ba355d67ce95a12425f4dcd
Gilbert West <gilbertwest@gmail.com>
2017-12-06 23:10:40 +0100

    Feature/invite refactor (#234)
    * docblock fix
    
    * Add new mailable dependency
    
    * Use wasRecentlyCreated
    
    this helps us figure out if it’s a new user or not
    
    * New mailable to notifications to existing users
    
    * Copy of the invite email
    
    needs to be adapted for existing users

---
+++ resources/views/emails/new-connection.blade.php
@@ -0,0 +1,9 @@
+@component('mail::message')
+<p>Geef mee kleur aan de herinneringen van {{ $patient }} met de Prisma app voor Android smartphones en
+    tablets.</p>
+<p><a href="{{ route('password.set',['token'=>$token]) }}" target="_blank">Bevestig je e-mail adres via deze
+        link</a> en kies daarna een wachtwoord.</p>
+<p>Werkt de link niet? Plak deze link in je internet browser: {{ route('password.set',['token'=>$token]) }}</p>
+<p>Meer weten over Prisma?<br>
+    www.prisma.care</p>
+@endcomponent
