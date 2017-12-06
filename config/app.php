273ee554e7e1d07c33acdd606321e8985c390d8c
Gilbert West <gilbertwest@gmail.com>
2017-12-06 21:21:26 +0100

    Add roll bar service provider

--- config/app.php
+++ config/app.php
@@ -183,6 +183,7 @@ return [
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
         Spatie\Backup\BackupServiceProvider::class,
         Mpociot\ApiDoc\ApiDocGeneratorServiceProvider::class,
+        Jenssegers\Rollbar\RollbarServiceProvider::class,
 
         /*
          * Application Service Providers...
